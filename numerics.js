d0b3aff94c3958cabf8472e840abefdc8f8b91ab
Abalhassan Sheikh <abalhassans@gmail.com>
2020-03-21 15:03:15 -0500

    Update numerics.js

--- numerics.js
+++ numerics.js
@@ -1,33 +1,20 @@
-// Numerics calculator can be extended in two ways:
-// via standard Javascript methods and variables
 
-this.myConst = 5;
-
-// hiddenVar is hidden element (to make public use this.hiddenVar)
-hiddenVar = 4;
-this.varB = [ 3, hiddenVar ];
 // via options.precision you can set the precision of decimal operations
 // see here for more http://mathjs.org/docs/reference/functions/format.html
-this.options={ notation: 'auto',  precision : 99,  exponential: { lower: 1e-99,  upper: 1e+99 }};
+this.options={ notation: 'auto',  precision : 99,  lowerExp: -99,  upperExp: 99 };
 
-hiddenFunc = function(x,y)
+this.decode_trim = function(x)
 {
-    return x - y;
-}
+return "Iref: " + ((x >> 11) & 31) + "\tSlope: " + ((x >> 6) & 31) + "\tVal: " + (x & 63);
+};
 
-this.test = function(x,y)
+
+
+this.limits = function(limit, tol)
 {
-    return (x+y) * hiddenFunc(x,y);
-}
+    return "Lower Limit:" + limit*(1-(tol/100))+"\tUpper Limit:" + limit*(1+(tol/100)); 
+};
 
-// or via math.js (see "http://mathjs.org/") compatible function definitions
-// by adding them to 'this.functions' array as strings
-this.functions=[
-    "hello(x) = [1,x,4]'",
-    "bye(x,y) = sin(x) + cos(y)"
-];
 
-// Now switch to calculator and try "hello(4)", "bye(5)", "test(2,3)" or "myConst"
-// Now you are ready, to go and add your own functions
 
 
